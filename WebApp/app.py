18bceab5c0332e63bb5429986b7bacba0d3282ce
KawshikManikantan <59600743+KawshikManikantan@users.noreply.github.com>
2020-10-08 15:11:11 +0530

    Day 1 Progresss: Basic Idea Obtained

--- WebApp/app.py
+++ WebApp/app.py
@@ -15,25 +15,6 @@ def algo1():
            f = open(os.path.join(__location__, 'code1.cpp'), "w")
            f.write(user)
            f.close()
-           # create a pipe to a child process
-           data, temp = os.pipe()
-
-           # write to STDIN as a byte object(convert string
-           # to bytes with encoding utf8)
-           os.write(temp, bytes("5 10\n", "utf-8"));
-           os.close(temp)
-
-           # store output of the program as a byte string in s
-           s = subprocess.check_output("cd "+'"'+__location__+'"'+g++ HelloWorld.cpp -o out2;./out2",
-                                       stdin=data, shell=True)
-
-           # decode s to a normal string
-           print(s.decode("utf-8"))
-           # s = subprocess.check_call("gcc.c -o out1;./out1",shell=True)
-           # subprocess.call(["cd ",__location__])
-           # subprocess.call(["pwd"])
-           # subprocess.call(["g++ code1.cpp -o code1"])
-           # subprocess.call("code1")
            print(user)
         except:
            print("No Result obtained")
